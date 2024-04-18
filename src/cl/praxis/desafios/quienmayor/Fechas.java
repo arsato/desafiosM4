23cc8180bb8f4df48c35d8c610edecd092bbd277
arsato <arsatoji@gmail.com>
2024-04-17 22:22:33 -0400

    feat: code refactor

--- src/cl/praxis/desafios/quienmayor/Fechas.java
+++ src/cl/praxis/desafios/quienmayor/Fechas.java
@@ -4,61 +4,26 @@ import java.util.Scanner;
 
 public class Fechas {
 
+    private static int counter = 0;
     public static void main(String[] args) {
-        String fecha1, fecha2;
-        int fecha1Day = 0, fecha2Day = 0, fecha1Month = 0, fecha2Month = 0, fecha1Year = 0, fecha2Year = 0;
-        boolean status;
+        int[] result1, result2;
+
         System.out.println("----------------");
         System.out.println("¿Quién es mayor?");
         System.out.println("----------------");
-        Scanner sc = new Scanner(System.in);
 
-        do {
-            System.out.println("Ingresa la primera fecha como DD/MM/AAAA:");
-            fecha1 = sc.nextLine();
-            if (evaluateString(fecha1)) {
-                fecha1Day = Integer.parseInt(fecha1.substring(0, 2));
-                fecha1Month = Integer.parseInt(fecha1.substring(3, 5));
-                fecha1Year = Integer.parseInt(fecha1.substring(6, 10));
-                if (fecha1Day > 0 && fecha1Day <= 31 && fecha1Month > 0 && fecha1Month <= 12 && fecha1Year > 1900 && fecha1Year <= 2024) {
-                    status = true;
-                } else {
-                    System.out.println("La fecha tiene un formato incorrecto (dias hasta 31, meses hasta 12 y años hasta 2024)");
-                    status = false;
-                }
-            } else {
-                status = false;
-            }
-        } while (!status);
+        result1 = evaluateDate();
+        result2 = evaluateDate();
 
-        do {
-            System.out.println("Ingresa la segunda fecha como DD/MM/AAAA:");
-            fecha2 = sc.nextLine();
-            if (evaluateString(fecha2)) {
-                fecha2Day = Integer.parseInt(fecha2.substring(0, 2));
-                fecha2Month = Integer.parseInt(fecha2.substring(3, 5));
-                fecha2Year = Integer.parseInt(fecha2.substring(6, 10));
-                if (fecha2Day > 0 && fecha2Day <= 31 && fecha2Month > 0 && fecha2Month <= 12 && fecha2Year > 1900 && fecha2Year <= 2024) {
-                    status = true;
-                } else {
-                    System.out.println("La fecha tiene un formato incorrecto (dias hasta 31, meses hasta 12 y años hasta 2024)");
-                    status = false;
-                }
-            } else {
-                status = false;
-            }
-        } while (!status);
-
-
-        if (fecha1Year > fecha2Year) {
+        if (result1[3] > result2[3]) {
             System.out.println("La persona 2 es mayor");
-        } else if (fecha1Year == fecha2Year) {
-            if (fecha1Month > fecha2Month) {
+        } else if (result1[3] == result2[3]) {
+            if (result1[2] > result2[2]) {
                 System.out.println("La persona 2 es mayor");
-            } else if (fecha1Month == fecha2Month) {
-                if (fecha1Day > fecha2Day) {
+            } else if (result1[2] == result2[2]) {
+                if (result1[1] > result2[1]) {
                     System.out.println("La persona 2 es mayor");
-                } else if (fecha2Day > fecha1Day) {
+                } else if (result2[1] > result1[1]) {
                     System.out.println("La persona 1 es mayor");
                 } else {
                     System.out.println("Las personas tienen la misma edad");
@@ -94,4 +59,30 @@ public class Fechas {
         }
     }
 
+    private static int[] evaluateDate() {
+        int[] result = new int[4];
+        String dateStr;
+        Scanner sc = new Scanner(System.in);
+
+        do {
+            System.out.println("Ingresa la fecha numero " + (counter+1) + " como DD/MM/AAAA:");
+            dateStr = sc.nextLine();
+            if (evaluateString(dateStr)) {
+                result[1] = Integer.parseInt(dateStr.substring(0, 2));
+                result[2] = Integer.parseInt(dateStr.substring(3, 5));
+                result[3] = Integer.parseInt(dateStr.substring(6, 10));
+                if (result[1] > 0 && result[1] <= 31 && result[2] > 0 && result[2] <= 12 && result[3] > 1900 && result[3] <= 2024) {
+                    result[0] = 1;
+                } else {
+                    System.out.println("La fecha tiene un formato incorrecto (dias hasta 31, meses hasta 12 y años hasta 2024)");
+                    result[0] = 0;
+                }
+            } else {
+                result[0] = 0;
+            }
+        } while (result[0] == 0);
+
+        counter++;
+        return result;
+    }
 }
