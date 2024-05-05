778db8ad707f9613a77ff414896645c326670ea8
arsato <arsatoji@gmail.com>
2024-05-04 20:06:28 -0400

    fix: Desafio escritura y lectura

--- src/cl/praxis/desafios/escrituraylectura/EscrituraLectura.java
+++ src/cl/praxis/desafios/escrituraylectura/EscrituraLectura.java
@@ -2,81 +2,68 @@ package cl.praxis.desafios.escrituraylectura;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class EscrituraLectura {
+
+    private final static String PATH = "src/cl/praxis/desafios/escrituraylectura/";
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Ingresa el nombre del directorio");
         String dirName = sc.nextLine();
-
         System.out.println("Ingresa el nombre del fichero");
         String fileName = sc.nextLine();
 
-        File newFile = crearArchivo(dirName, fileName);
+        crearArchivo(dirName, fileName);
 
-        if (newFile.exists()) {
-            escribirFichero(newFile);
-            System.out.println("Ingresa el nombre del fichero a buscar");
-            String fichero = sc.nextLine();
-            fichero = dirName.concat("/").concat(fichero);
-            System.out.println("Ingresa el texto a buscar");
-            String texto = sc.nextLine();
-            buscarTexto(fichero, texto);
-        }
+        System.out.println("Ingresa el nombre del fichero a buscar");
+        String fichero = sc.nextLine();
+        fichero = PATH.concat(dirName).concat("/").concat(fichero).concat(".txt");
+        System.out.println("Ingresa el texto a buscar");
+        String texto = sc.nextLine();
+        buscarTexto(fichero, texto);
     }
 
-    private static File validarArchivo(String pathName, String type) {
-        String dirOrFile = pathName.contains(".txt") ? "Fichero" : "Directorio";
-        File path = new File(pathName);
-        boolean state;
-        if (type.equals("read")) {
-            if (!path.exists()) {
-                System.out.println("El fichero ingresado no existe");
-            } else {
-                System.out.println("Fichero existente");
-            }
-        } else {
-            if (!path.exists()) {
-                try {
-                    state = pathName.contains(".txt") ? path.createNewFile() : path.mkdir();
-                    System.out.printf("%s creado%n", dirOrFile);
-                    if (!state) {
-                        System.out.printf("%s no creado%n", dirOrFile);
-                    }
-                } catch (IOException e) {
-                    System.out.println("IO EXCEPTION");
-                }
-            } else {
-                System.out.printf("%s ya existe%n", dirOrFile);
-            }
-        }
-        return path;
-    }
+    private static void crearArchivo(String directorio, String fichero) {
+        String fullPath = PATH.concat(directorio).concat("/").concat(fichero).concat(".txt");
+        boolean dirstate = true, state;
 
-    private static File crearArchivo(String directorio, String fichero) {
-        String path = "src/cl/praxis/desafios/escrituraylectura/";
-        fichero = fichero.concat(".txt");
-        directorio = path.concat(directorio + "/");
-        validarArchivo(directorio, "create");
-        return validarArchivo(directorio.concat(fichero), "create");
+        File path = new File(fullPath);
+
+        if(!path.getParentFile().exists()) {
+            System.out.println("Directorio no existente");
+            state = path.getParentFile().mkdir();
+            if(state){
+                System.out.println("Directorio creado");
+            }else{
+                System.out.println("Error al crear el directorio");
+            }
+            dirstate = state;
+        }else
+            System.out.println("Directorio existente");
+
+        if (dirstate && !path.exists()) {
+            try {
+                state = path.createNewFile();
+                if(state){
+                    System.out.println("Fichero creado");
+                    escribirFichero(path);
+                }else
+                    System.out.println("Error al crear el fichero");
+            } catch (IOException e) {
+                System.out.println("Ocurrió un error de I/O");
+            }
+        }else{
+            System.out.println("Fichero existente");
+        }
     }
 
     private static void escribirFichero(File file) {
         ArrayList<String> lista = new ArrayList<>();
-        lista.add("Perro");
-        lista.add("Gato");
-        lista.add("Juan");
-        lista.add("Daniel");
-        lista.add("Juan");
-        lista.add("Gato");
-        lista.add("Perro");
-        lista.add("Camila");
-        lista.add("Daniel");
-        lista.add("Camila");
-
+        Collections.addAll(lista,"Perro", "Gato", "Juan", "Daniel", "Juan", "Gato", "Perro", "Camila", "Daniel", "Camila");
         try {
             FileWriter fileW = new FileWriter(file);
             BufferedWriter bufferedW = new BufferedWriter(fileW);
@@ -85,26 +72,24 @@ public class EscrituraLectura {
                 bufferedW.newLine();
             }
             bufferedW.close();
-            System.out.println("Fichero escrito correctamente");
+            System.out.println("Datos registrados correctamente");
         } catch (IOException e) {
-            System.out.println("IO EXCEPTION");
+            System.out.println("Ocurrió un error de I/O");
         }
     }
 
     private static void buscarTexto(String nombreFichero, String texto) {
-        String path = "src/cl/praxis/desafios/escrituraylectura/";
-        nombreFichero = path.concat(nombreFichero).concat(".txt");
         ArrayList<String> lista = new ArrayList<>();
-        if (validarArchivo(nombreFichero, "read").exists()) {
+        if (new File(nombreFichero).exists()) {
             try {
                 FileReader fr = new FileReader(nombreFichero);
                 BufferedReader br = new BufferedReader(fr);
                 br.lines().filter(s -> s.equals(texto)).forEach(lista::add);
-                System.out.println("Cantidad de coincidencias del texto (" + texto + "): " + lista.size());
-                br.close();
+                System.out.println("Cantidad de coincidencias del texto '" + texto + "' -> " + lista.size());
             } catch (IOException e) {
-                System.out.println("IO EXCEPTION");
+                System.out.println("Ocurrió un error de I/O");
             }
-        }
+        }else
+            System.out.println("El fichero ingresado no existe");
     }
 }
